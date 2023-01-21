52c76feae900d2de53289d2fbe99ff7f7da384b3
matthardy11 <matthardy2560@gmail.com>
2023-01-21 16:15:34 +0400

    complete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая принимает на вход трехзначное число 
+// и на выхоже показывает вторую цифру этого числа
+
+System.Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numb1 = number / 10;
+int numb2 = numb1 % 10;
+
+System.Console.WriteLine(numb2);

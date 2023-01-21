86c0c6936d6046ff68d6a0309c821cc43df91397
matthardy11 <matthardy2560@gmail.com>
2023-01-21 17:09:53 +0400

    complete task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+System.Console.WriteLine("Введите номер дня недели (1 - 7): ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>7)
+{
+  System.Console.WriteLine("Такого дня недели нет");
+}
+else if (number == 6 || number == 7)
+{
+  System.Console.WriteLine("Выходной");
+}
+else
+{
+  System.Console.WriteLine("Не выходной");
+}
